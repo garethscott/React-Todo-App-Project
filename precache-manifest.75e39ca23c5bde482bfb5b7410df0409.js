967aea72aa1cb0130fd76ba0436f1bb864d97bc7
garethscott <59648675+garethscott@users.noreply.github.com>
2020-10-19 19:43:36 +0100

    Updates

---
+++ precache-manifest.75e39ca23c5bde482bfb5b7410df0409.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e43322310be1e7376ad4a2d4a2c68c9b",
+    "url": "/React-Todo-App-Project/index.html"
+  },
+  {
+    "revision": "fa82b04d48cb7bf172d4",
+    "url": "/React-Todo-App-Project/static/css/main.7ee09635.chunk.css"
+  },
+  {
+    "revision": "8c0826d9d91886d27d3f",
+    "url": "/React-Todo-App-Project/static/js/2.5e31355d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todo-App-Project/static/js/2.5e31355d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa82b04d48cb7bf172d4",
+    "url": "/React-Todo-App-Project/static/js/main.2aa6c946.chunk.js"
+  },
+  {
+    "revision": "2575a8a06f8b5d5c96ca",
+    "url": "/React-Todo-App-Project/static/js/runtime-main.9ffc8f45.js"
+  }
+]);
